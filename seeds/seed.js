5fea823d1f6d9e91ff8779c53884db8be7a9b505
DBrush <coldnebraska@gmail.com>
2023-12-30 12:38:03 -0600

    added seeds structure and data

--- seeds/seed.js
+++ seeds/seed.js
@@ -1,8 +1,8 @@
 const sequelize = require('../config/connection');
-const { User, Project } = require('../models');
+const { User, Post } = require('../models');
 
 const userData = require('./userData.json');
-const projectData = require('./blogPosts.json');
+const postData = require('./blogPosts.json');
 
 const seedDatabase = async () => {
   await sequelize.sync({ force: true });
@@ -12,10 +12,9 @@ const seedDatabase = async () => {
     returning: true,
   });
 
-  for (const project of projectData) {
-    await Project.create({
-      ...project,
-      user_id: users[Math.floor(Math.random() * users.length)].id,
+  for (const post of postData) {
+    await Post.create({
+      ...post
     });
   }
 
